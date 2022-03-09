464f99f3fb158c889918cbbd5432405a5464075e
Manuel Baltierra <adanjetzaid@gmail.com>
2022-03-09 14:05:35 -0600

    Variables for bucket template

---
+++ dealer/variables.tf
@@ -0,0 +1,12 @@
+variable "region" {
+    type = string
+    default = "us-east-2"
+}
+variable "bucket" {
+    type = string
+    default = "mbaltierrambucket1"
+}
+
+variable "content" {    
+    type = string
+}   
