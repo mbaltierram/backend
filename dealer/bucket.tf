6a41334b140b25096632de1eec4557824a780460
Manuel Baltierra <adanjetzaid@gmail.com>
2022-03-09 14:29:49 -0600

    Variables for bucket template

--- dealer/bucket.tf
+++ dealer/bucket.tf
@@ -12,10 +12,10 @@ provider "aws" {
 
 resource "aws_s3_bucket" "bucket1" {
   bucket = var.bucket
-  acl = "public-read"
-  website {
-      index_document = "index.html"
-  }
+#  acl = "public-read"
+#  website {
+#      index_document = "index.html"
+#  }
 }
 
 /*------------content------------*/
