4ee6092c3e9393426c307f6d8fa81d3e1c41d3de
Manuel Baltierra <adanjetzaid@gmail.com>
2022-03-09 12:15:40 -0600

    Template Exercise

---
+++ instance.tf
@@ -0,0 +1,21 @@
+terraform {
+    required_providers {
+        aws = {
+            source  = "hashicorp/aws"
+            version = "~> 3.0"
+        }
+    }
+}
+
+provider "aws" {
+    region = "us-east-2"
+}
+
+
+resource "aws_instance" "hello-instance" {
+    ami = "ami-0fb653ca2d3203ac1"
+    instance_type = "t2.micro"
+    tags = {
+        Name = "hello-instance"
+    }
+}
